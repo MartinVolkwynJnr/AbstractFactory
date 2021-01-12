bc21394f16fc9d2e78200ad3a60062d9040c9f53
Martin <volkwyncarl@gmail.com>
2021-01-12 19:40:25 +0200

    Add files via upload

---
+++ AbstractFactory/AbstractFactory/Interfaces/AbstractFactory/IContinentFactory.cs
@@ -0,0 +1,8 @@
+﻿namespace AbstractFactory.Interfaces
+{
+    public interface IContinentFactory
+    {
+        IHerbivore CreateHerbivore();
+        ICarnivore CreateCarnivore();
+    }
+}
---
+++ AbstractFactory/AbstractFactory/MainApp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using AbstractFactory.Objects;
+
+namespace AbstractFactory
+{
+    class MainApp
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+
+            // Create and run the African animal world
+            var africa = new AnimalWorld<Africa>();
+            africa.RunFoodChain();
+
+            // Create and run the American animal world
+            var america = new AnimalWorld<America>();
+            america.RunFoodChain();
+
+            Console.ReadKey();
+        }
+    }
+}
